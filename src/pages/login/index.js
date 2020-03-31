88274e11a3c586fed939dcf16e44286e17609201
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-03-30 23:06:58 -0300

    Criada estrutura visual da tela de login.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: João Pedro Soares <jpcirqueira81@gmail.com>

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+// import {FiLogIn} from 'react-icons/fi';
 
 import styles from './styles.css';
 
 export default function Login() {
     return(
         <div className="container" >
-            <form className="form" onSubmit={()=>{}}>
-                <input className="campo" type="text" placeholder="E-mail"/>
-                <input className="campo" type="text" placeholder="Senha"/>
-                <button className="botaoVermelho" onClick={()=>{}}>Entrar</button>
-            </form>
+            <section className="caixaDeCampos">
+                <form  onSubmit={()=>{}}>
+                    <h1>Seja bem vindo<br/>ao<br/>Pizza js</h1>
+                    <input className="campo" type="text" placeholder="E-mail"/>
+                    <input className="campo" type="text" placeholder="Senha"/>
+                    <button className="botaoVermelho" onClick={()=>{}}>Entrar</button>
+                    
+                </form>
+                <Link className="link" to ="/">Não tenho cadastro 
+    
+                </Link>
+                
+            </section>
         </div>
     );
 }
