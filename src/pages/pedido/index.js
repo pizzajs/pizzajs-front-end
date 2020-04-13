6365a59be9e97af1f3a508f695d81603608a1614
João Soares <jpcirqueira81@gmail.com>
2020-04-12 23:57:19 -0300

    ajustado tela de pedido.

--- src/pages/pedido/index.js
+++ src/pages/pedido/index.js
@@ -12,28 +12,61 @@ export default function Pedido() {
                 <Cabecalho />
             </div>      
             <div className = "caixaPricipal">
-                <h1>Meu Pedido</h1>
-                <div className="caixapizza">
-                    <div className="pizzas">
-                        <h1>Pizzas</h1>
-                        <ul>
-                            <li>minha pizza</li>
-                            <li>calabresa</li>
-                            <li>mussarela</li>
-                        </ul>
-                        <h1>valor total</h1>
-                    </div>
-                </div>
-                <div className="caixabebida">
-                    <div className="bebida">
-                        <h1>Bebidas</h1>
-                        <ul>
-                            <li>coca-cola</li>
-                            <li>guarana</li>
-                            <li>fanta</li>
-                        </ul>
+                <h1 className="titulo">Meu Pedido</h1>
+                <div className="caixas">
+                    <span className="caixapizza">
+                       
+                        <div className="pizzas">
+                            <h1>Pizzas</h1>
+                            <ul>
+                                <li>minha pizza</li>
+                                <li>calabresa</li>
+                                <li>mussarela</li>
+                                <li>minha pizza</li>
+                                <li>calabresa</li>
+                                <li>mussarela</li>  
+                                <li>minha pizza</li>
+                                <li>calabresa</li>
+                                <li>mussarela</li>  
+                                <li>minha pizza</li>
+                                <li>calabresa</li>
+                                <li>mussarela</li>  
+                                <li>minha pizza</li>
+                                <li>calabresa</li>
+                                <li>mussarela</li>                        
+                            </ul>
+                        </div> 
+                        <h1 className="valortotal">Valor total: 53,72</h1>
+                    </span>
+                    <span className="caixabebida">  
+                        <div className="bebida">
+                            <h1>Bebidas</h1>
+                            <ul>
+                                <li>coca-cola</li>
+                                <li>guarana</li>
+                                <li>fanta</li>
+                                <li>coca-cola</li>
+                                <li>guarana</li>
+                                <li>fanta</li>
+                                <li>coca-cola</li>
+                                <li>guarana</li>
+                                <li>fanta</li>
+                                <li>coca-cola</li>
+                                <li>guarana</li>
+                                <li>fanta</li>
+                                <li>coca-cola</li>
+                                <li>guarana</li>
+                                <li>fanta</li>
+                                <li>coca-cola</li>
+                                <li>guarana</li>
+                                <li>fanta</li>
+                                <li>coca-cola</li>
+                                <li>guarana</li>
+                                <li>fanta</li>
+                            </ul>
+                        </div>
                         <button className="redbotao">Finalizar pedido</button>
-                    </div>
+                    </span>
                 </div>
             </div>
         </div>
