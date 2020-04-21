82b03133931ba366154720117e249dd39cbe38d6
João Soares <jpcirqueira81@gmail.com>
2020-04-20 21:47:24 -0300

    adicionado estilo a tela home.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,13 +10,33 @@ export default function Home() {
             <Cabecalho />
             <div className="paginainicial">
                 <div className="navegacao">
-                    <h1>Montar Pizza</h1>
-                    <h1>Bebidas</h1>
+                    <h1 className="montarpizza">Montar Pizza</h1>
+                    <h1 className="escolherbebida">Bebidas</h1>
                 </div>
-                <div className="caixapizza">
-                    <h1 className="nomepizza">Calabresa</h1>
-                    <img className =" imagem"src={imgPizza}/>
+                <div className="pizzasprontas">
+                    <div className="caixapizza">
+                        <h1 className="nomepizza">Calabresa</h1>
+                        <img className ="imagem"src={imgPizza}/>
+                    </div>
+                    <div className="caixapizza">
+                        <h1 className="nomepizza">Calabresa</h1>
+                        <img className ="imagem"src={imgPizza}/>
+                    </div>
+                    <div className="caixapizza">
+                        <h1 className="nomepizza">Calabresa</h1>
+                        <img className ="imagem"src={imgPizza}/>
+                    </div>
+                    <div className="caixapizza">
+                        <h1 className="nomepizza">Calabresa</h1>
+                        <img className ="imagem"src={imgPizza}/>
+                    </div>
+                    <div className="caixapizza">
+                        <h1 className="nomepizza">Calabresa</h1>
+                        <img className ="imagem"src={imgPizza}/>
+                    </div>
                 </div>
+                
+                
             </div>
         </div>
     );
