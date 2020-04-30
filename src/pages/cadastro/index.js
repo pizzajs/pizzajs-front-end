107c6e6d5ec775807a6f54422a6cb3e89e11dd7a
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-04-29 23:38:20 -0300

    Criado service api. Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com> Co-authored-by: João Pedro Soares <jpcirqueira81@gmail.com>

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -5,9 +5,40 @@ import Cabecalho from '../../utils/cabecalho/index';
 import { MdArrowBack } from "react-icons/md";
 import { Form, Input } from '@rocketseat/unform';
 
+import api from '../../services/api';
+
+
+
 export default function Cadastro() {
-    function dadosCadastro(dados){
-        console.log(dados);
+    
+    async function Cadastrar(dados){
+        // const headers={
+        //     'Content-Type': 'application/json; charset=utf-8',
+        //     'Access-Control-Allow-Origin': '*'
+        // }
+
+        if(dados.senha === dados.confirmarsenha) {
+            // application/json;
+            const data = {
+                'nome': dados.nome,
+                'senha': dados.senha,
+                'endereco': dados.endereco,
+                'email': dados.email,
+                'telefone': dados.telefone,
+                'admin': false
+            };
+            
+            try {
+                const response =  await api.post('/users',data);
+            } catch (error) {
+                alert('deu errado')
+            }
+        }else {
+            alert('A digite senhas iguais!');
+        }
+        
+        
+        // api.post('cadastro', data)
     }
 
     return(
@@ -15,12 +46,13 @@ export default function Cadastro() {
             <div className="header">
                 <Cabecalho />
             </div>
-            <Form onSubmit={dadosCadastro}className="cadastro">
+            <Form onSubmit={Cadastrar}className="cadastro">
                 <div className="caixaprincipal">
                     <h1 className="titulo"> Pizza js</h1>
-                    <Input name= "Nome" type="text" placeholder="Nome" className="input"/>
+                    <Input name= "nome" type="text" placeholder="Nome" className="input"/>
                     <Input name="email" type="email" placeholder="E-mail" className="input"/>
-                    <Input name= "telefone" type="tel" placeholder="Telefone" className="input"/>
+                    <Input name= "telefone" type="text" placeholder="Telefone" className="input"/>
+                    <Input name= "endereco" type="text" placeholder="Endereco" className="input"/>
                     <Input name= "senha" type="password" placeholder="Senha" className="input"/>
                     <Input name= "confirmarsenha" type="password" placeholder="Confirmar Senha" className="input"/>
                     <button className="botaovermelho">Cadastrar</button>
