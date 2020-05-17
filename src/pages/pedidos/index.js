9efa2b0149a3cde6b35fbc2cef7e557e44441e43
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-05-17 17:51:06 -0300

    Criadas as funções para formatar e renderizar pedidos

--- src/pages/pedidos/index.js
+++ src/pages/pedidos/index.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 import Cabecalho from '../../utils/cabecalho';
-
+import api from '../../services/api';
 import './styles.css';
 
 export default function Pedidos(){
+    const [pedidos, setPedidos]= useState([]);
+    const [total, setTotal]= useState(0);
+
+    useEffect(()=>{
+        async function getPedidos(){
+            const response = await api.get('/pedidos/todos');
+            setPedidos(response.data);
+        }
+
+        getPedidos();
+    },[]);
+
+    function formataData(data){
+        const aux = new Date(data);
+        const dia = String(aux.getUTCDate());
+        const mes = String(aux.getMonth());
+        const ano = String(aux.getUTCFullYear());
+        const hora = String(aux.getHours());
+        const min = aux.getUTCMinutes();
+        return (dia + '/'+mes +'/'+ ano+ ' ' + hora+'h:'+min+'min');
+    }
+    function formataLista(lista){
+        const aux = lista.map(item=>{
+            return (String("- "+item+" " ))
+        })
+        return aux;
+    }
+
+    function ListaPedido(props){
+        let pizzas = props.pizzas;
+        let bebidas = props.bebidas;
+        pizzas = pizzas.map((pizza) => 
+            <div key={pizza.id}>
+                <h2>Sabor: {pizza.sabor}</h2>
+                <p>
+                    Ingredientes: { formataLista( pizza.ingredientes_padrao)}<br/>
+                    Preço: R$ {pizza.preco}<br/>
+                    {/* Ingredientes extras: {pizza.ingredientes_extra}  */}
+                </p>
+            </div>
+        );
+
+        bebidas = bebidas.map((bebida)=>(
+            <div key={bebida.id}>
+                <h2>Sabor: {bebida.nome}</h2>
+                <p>
+                    Quantidade: {bebida.quantidade}<br/>
+                    Preço: R$ {bebida.quantidade * bebida.preco}<br/>
+                </p>
+            </div>
+        ));
+
+        return (
+            <div >
+                <h1>Pizza(s)</h1>
+                {pizzas}
+                <h1>Bebida(s)</h1>
+                {bebidas}
+            </div>
+        );
+    }
+
+
     return(
         <div className="produto">
             <header className="header">
@@ -13,8 +76,22 @@ export default function Pedidos(){
                 <h1>Todos os pedidos</h1>
             </div>
             <ul className="todosPedidos">
+                {
+                    pedidos.map(pedido =>(
+                    <li key={pedido.id} className="cardPedido">
+                        <div >
+                            <h1>Pedido {pedido.id}</h1>
+                            <h2>Hora do pedido: {formataData(pedido.createdAt)}</h2>
+                            <ListaPedido  pizzas={pedido.pizzas} bebidas={ pedido.bebidas}/>
+                            <h1>Preço total: R$ {pedido.preco}</h1>
+
+                        </div>
+                    </li>
+                        
+                    ))
+                }
                 
-                    <li>
+                    {/* <li>
                         <div className="cardPedido">
                             <h1>Pedido 1</h1>
                             <p>
@@ -79,7 +156,7 @@ export default function Pedidos(){
                             </p>
 
                         </div>
-                    </li>
+                    </li> */}
                     
                     
                 
