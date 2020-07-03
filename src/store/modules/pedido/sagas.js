9b5874fcbc580bc90452b8eccb48c1246ce317c6
João Soares <jpcirqueira81@gmail.com>
2020-07-03 19:21:26 -0300

    adicionado logica para incluir pizzas customizadas ao pedido.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>

--- src/store/modules/pedido/sagas.js
+++ src/store/modules/pedido/sagas.js
@@ -7,9 +7,16 @@ import history from '../../../services/history'
 
 export function* concluirPedido({payload}){
     const { bebidaId, pizzas_ids, pizzas_customizadas, preco } = payload;
+    let pizzasid = []
+
+    for(var pizza in pizzas_customizadas){
+        pizzasid.push(pizzas_customizadas[pizza].ingredientesIds)
+    }
+
     
     try {
         yield call(api.post, 'pedidos', {
+            pizzas_customizadas: pizzasid,
             pedido_ativo: true,
             preco: preco,
             bebidas_id: bebidaId,
